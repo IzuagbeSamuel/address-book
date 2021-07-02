d69b9ff0030664f82f9e4bdb6d62878aaffdbb2b
Izuagbe Samuel <izuagbesamuel73@gmail.com>
2021-07-02 03:23:12 +0100

    cleared field input for other email and address

--- js/script.js
+++ js/script.js
@@ -29,11 +29,11 @@ AddressBook.prototype.UpdateContact = function (contact) {
   this.contacts[id].firstName = contact.firstName;
   this.contacts[id].lastName = contact.lastName;
   this.contacts[id].phoneNumber = contact.phoneNumber;
-  this.contacts[id].email = contact.email
-  this.contacts[id].address = contact.address
-  this.contacts[id].workAddress = contact.workAddress
-  this.contacts[id].otherAddress = contact.otherAddress
-  this.contacts[id].workEmail = contact.workEmail
+  this.contacts[id].email = contact.email;
+  this.contacts[id].address = contact.address;
+  this.contacts[id].workAddress = contact.workAddress;
+  this.contacts[id].otherAddress = contact.otherAddress;
+  this.contacts[id].workEmail = contact.workEmail;
   return true;
 };
 
@@ -61,7 +61,7 @@ Contact.prototype.fullName = function () {
   return this.firstName + ' ' + this.lastName;
 };
 
-                                        // User Interface Logic
+// User Interface Logic
 let addressBook = new AddressBook();
 
 function displayContactDetails(addressBookToDisplay) {
@@ -95,11 +95,11 @@ function attachContactListeners() {
   $('ul#contacts').on('click', 'li', function () {
     showContact(this.id);
   });
-  $('#buttons').on('click', '.deleteButton', function() {
+  $('#buttons').on('click', '.deleteButton', function () {
     addressBook.deleteContact(this.id);
     $('#show-contact').hide();
-    displayContactDetails(addressBook)
-  })
+    displayContactDetails(addressBook);
+  });
 }
 
 $(document).ready(function () {
@@ -113,19 +113,31 @@ $(document).ready(function () {
     const inputtedAddress = $('input#new-address').val();
     const inputtedWorkAddress = $('input#new-work-address').val();
     const inputtedOtherAddress = $('input#new-other-address').val();
-    const inputtedWorkEmail= $('input#new-work-email').val();
+    const inputtedWorkEmail = $('input#new-work-email').val();
     $('input#new-first-name').val('');
     $('input#new-last-name').val('');
     $('input#new-phone-number').val('');
     $('input#new-email').val('');
     $('input#new-address').val('');
-    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmail, inputtedAddress, inputtedOtherAddress, inputtedWorkAddress, inputtedWorkEmail);
+    $('input#new-work-address').val('');
+    $('input#new-other-address').val('');
+    $('input#new-work-email').val('');
+    let newContact = new Contact(
+      inputtedFirstName,
+      inputtedLastName,
+      inputtedPhoneNumber,
+      inputtedEmail,
+      inputtedAddress,
+      inputtedOtherAddress,
+      inputtedWorkAddress,
+      inputtedWorkEmail,
+    );
     addressBook.addContact(newContact);
     displayContactDetails(addressBook);
   });
 
-  document.getElementById('dropDown2').addEventListener('click', showEmail)
-  document.getElementById('dropDown').addEventListener('click', showAddress)
+  document.getElementById('dropDown2').addEventListener('click', showEmail);
+  document.getElementById('dropDown').addEventListener('click', showAddress);
   function showAddress() {
     document.getElementById('other-addresses').style.display = 'block';
   }
